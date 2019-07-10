9d54817452750ad53a56d58635ee68848a6b0d4d
Youngsu Park <youngsu999@gmail.com>
2019-07-10 15:24:24 +0900

    #1 remove default parameter load

--- resnet_152.py
+++ resnet_152.py
@@ -13,6 +13,7 @@ from custom_layers.scale_layer import Scale
 import sys
 sys.setrecursionlimit(3000)
 
+
 def identity_block(input_tensor, kernel_size, filters, stage, block):
     '''The identity_block is the block that has no conv layer at shortcut
     # Arguments
@@ -35,7 +36,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
 
     x = ZeroPadding2D((1, 1), name=conv_name_base + '2b_zeropadding')(x)
     x = Conv2D(nb_filter2, (kernel_size, kernel_size),
-                      name=conv_name_base + '2b', use_bias=False)(x)
+               name=conv_name_base + '2b', use_bias=False)(x)
     x = BatchNormalization(epsilon=eps, axis=bn_axis, name=bn_name_base + '2b')(x)
     x = Scale(axis=bn_axis, name=scale_name_base + '2b')(x)
     x = Activation('relu', name=conv_name_base + '2b_relu')(x)
@@ -47,6 +48,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     x = add([x, input_tensor], name='res' + str(stage) + block)
     x = Activation('relu', name='res' + str(stage) + block + '_relu')(x)
     return x
+
 
 def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
     '''conv_block is the block that has a conv layer at shortcut
@@ -66,14 +68,14 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     scale_name_base = 'scale' + str(stage) + block + '_branch'
 
     x = Conv2D(nb_filter1, (1, 1), strides=strides,
-                      name=conv_name_base + '2a', use_bias=False)(input_tensor)
+               name=conv_name_base + '2a', use_bias=False)(input_tensor)
     x = BatchNormalization(epsilon=eps, axis=bn_axis, name=bn_name_base + '2a')(x)
     x = Scale(axis=bn_axis, name=scale_name_base + '2a')(x)
     x = Activation('relu', name=conv_name_base + '2a_relu')(x)
 
     x = ZeroPadding2D((1, 1), name=conv_name_base + '2b_zeropadding')(x)
     x = Conv2D(nb_filter2, (kernel_size, kernel_size),
-                      name=conv_name_base + '2b', use_bias=False)(x)
+               name=conv_name_base + '2b', use_bias=False)(x)
     x = BatchNormalization(epsilon=eps, axis=bn_axis, name=bn_name_base + '2b')(x)
     x = Scale(axis=bn_axis, name=scale_name_base + '2b')(x)
     x = Activation('relu', name=conv_name_base + '2b_relu')(x)
@@ -83,7 +85,7 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     x = Scale(axis=bn_axis, name=scale_name_base + '2c')(x)
 
     shortcut = Conv2D(nb_filter3, (1, 1), strides=strides,
-                             name=conv_name_base + '1', use_bias=False)(input_tensor)
+                      name=conv_name_base + '1', use_bias=False)(input_tensor)
     shortcut = BatchNormalization(epsilon=eps, axis=bn_axis, name=bn_name_base + '1')(shortcut)
     shortcut = Scale(axis=bn_axis, name=scale_name_base + '1')(shortcut)
 
@@ -91,7 +93,8 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     x = Activation('relu', name='res' + str(stage) + block + '_relu')(x)
     return x
 
-def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
+
+def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None, weights_path=None):
     """
     Resnet 152 Model for Keras
 
@@ -112,11 +115,11 @@ def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
     # Handle Dimension Ordering for different backends
     global bn_axis
     if K.image_dim_ordering() == 'tf':
-      bn_axis = 3
-      img_input = Input(shape=(img_rows, img_cols, color_type), name='data')
+        bn_axis = 3
+        img_input = Input(shape=(img_rows, img_cols, color_type), name='data')
     else:
-      bn_axis = 1
-      img_input = Input(shape=(color_type, img_rows, img_cols), name='data')
+        bn_axis = 1
+        img_input = Input(shape=(color_type, img_rows, img_cols), name='data')
 
     x = ZeroPadding2D((3, 3), name='conv1_zeropadding')(img_input)
     x = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', use_bias=False)(x)
@@ -130,12 +133,12 @@ def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
     x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
 
     x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
-    for i in range(1,8):
-      x = identity_block(x, 3, [128, 128, 512], stage=3, block='b'+str(i))
+    for i in range(1, 8):
+        x = identity_block(x, 3, [128, 128, 512], stage=3, block='b'+str(i))
 
     x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
-    for i in range(1,36):
-      x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b'+str(i))
+    for i in range(1, 36):
+        x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b'+str(i))
 
     x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
@@ -147,14 +150,14 @@ def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
 
     model = Model(img_input, x_fc)
 
-    if K.image_dim_ordering() == 'th':
-      # Use pre-trained weights for Theano backend
-      weights_path = 'models/resnet152_weights_th.h5'
-    else:
-      # Use pre-trained weights for Tensorflow backend
-      weights_path = 'models/resnet152_weights_tf.h5'
+    # if K.image_dim_ordering() == 'th':
+    #   # Use pre-trained weights for Theano backend
+    #   weights_path = 'models/resnet152_weights_th.h5'
+    # else:
+    #   # Use pre-trained weights for Tensorflow backend
+    #   weights_path = 'models/resnet152_weights_tf.h5'
 
-    model.load_weights(weights_path, by_name=True)
+    # model.load_weights(weights_path, by_name=True)
 
     # Truncate and replace softmax layer for transfer learning
     # Cannot use model.layers.pop() since model is not of Sequential() type
@@ -164,20 +167,20 @@ def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
     x_newfc = Dense(num_classes, activation='softmax', name='fc8')(x_newfc)
 
     model = Model(img_input, x_newfc)
-
     # Learning rate is changed to 0.001
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
+
 if __name__ == '__main__':
 
     # Example to fine-tune on 3000 samples from Cifar10
 
-    img_rows, img_cols = 224, 224 # Resolution of inputs
+    img_rows, img_cols = 224, 224  # Resolution of inputs
     channel = 3
-    num_classes = 10 
+    num_classes = 10
     batch_size = 8
     epochs = 10
 
